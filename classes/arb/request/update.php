78377fd797192855da7558d433e3c4990e9af9d8
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 07:01:26 -0700

    finishing up the update request

--- classes/arb/request/update.php
+++ classes/arb/request/update.php
@@ -3,4 +3,57 @@
 class ARB_Request_Update extends ARB_Request {
 
 	protected $_view_path = 'auth-arb/update_subscription.xml';
+
+	public function validation_object()
+	{
+		return parent::validation_object()
+			 // Merchant-assigned name for the subscription
+			 // Optional
+			->rule('subscription_name', 'max_length', array(50))
+			// Number of billing occurrences or payments for the subscription.
+			// To submit a subscription with no end date (an ongoing subscription), this
+			// field must be submitted with a value of "9999."
+			// If a trial period is specified, this number should include the Trial Occurrences.
+			->rule('total_occurrences', 'max_length', array(4))
+			// @TODO: implement trial occurrences
+			// --
+			// The amount to be billed to the customer for each payment in the subscription.
+			// If a trial period is specified, this is the amount that will be charged after
+			// the trial payments are completed.
+			->rule('amount', 'max_length', array(15))
+			// @TODO: implement trial_amount
+			// --
+			// @TODO: implement bank/cc choices
+			// --
+			->rule('card_number', 'min_length', array(13))
+			->rule('card_number', 'max_length', array(16))
+			// YYYY-MM
+			->rule('card_expiration_date', 'exact_length', array(7))
+			// The three- or four-digit card code on the back of most credit
+			// cards, on the front for American Express.
+			// This element should only be included when the merchant has set the card code
+			// value field to required in the account settings. The value itself is never validated.
+			->rule('card_code', 'min_length', array(3))
+			->rule('card_code', 'max_length', array(4))
+			// Merchant-assigned invoice number for the subscription.
+			// Optional
+			// The invoice number will be associated with each payment in the subscription.
+			->rule('invoice_number', 'max_length', array(20))
+			// Description of the subscription.
+			// Optional
+			// The description will be associated with each payment in the subscription.
+			->rule('invoice_description', 'max_length', array(255))
+			// Merchant-assigned identifier for the customer.
+			// Optional
+			->rule('user_id', 'max_length', array(20))
+			// The customer’s email address.
+			// Optional
+			->rule('user_email', 'max_length', array(255))
+			// The customer’s phone number.
+			// Optional
+			->rule('user_phone', 'max_length', array(25))
+			// 
+			->rule('billing_first_name', 'max_length', array(25))
+			->rule('billing_last_name', 'max_length', array(25));
+	}
 }
--- views/auth-arb/update_subscription.xml.php
+++ views/auth-arb/update_subscription.xml.php
@@ -4,13 +4,20 @@
  * Parameters:
  * - api_login_id
  * - transaction_key
+ * - subscription_id
  * - ref_id
- * - name
+ * - subscription_name
  * - amount
  * - card_number
- * - expiration_date
- * - first_name
- * - last_name
+ * - card_expiration_date
+ * - card_code
+ * - invoice_number
+ * - invoice_description
+ * - user_id
+ * - user_email
+ * - user_phone
+ * - billing_first_name
+ * - billing_last_name
  */
 echo "<?xml version='1.0' encoding='utf-8'?>"; ?>
 <ARBUpdateSubscriptionRequest xmlns='AnetApi/xml/v1/schema/AnetApiSchema.xsd'>
@@ -18,19 +25,56 @@ echo "<?xml version='1.0' encoding='utf-8'?>"; ?>
 		<name><?php echo $api_login_id; ?></name>
 		<transactionKey><?php echo $transaction_key; ?></transactionKey>
 	</merchantAuthentication>
+	<?php if (isset($ref_id)): ?>
 	<refId><?php echo $ref_id; ?></refId>
+	<?php endif; ?>
+	<subscriptionId><?php echo $subscription_id; ?></subscriptionId>
 	<subscription>
-		<name><?php echo $name; ?></name>
-		<subscriptionId><?php echo $subscription_id; ?></subscriptionId>
+		<?php if (isset($subscription_name)): ?>
+		<name><?php echo $subscription_name; ?></name>
+		<?php endif; ?>
+		<?php if (isset($amount)): ?>
+		<amount><?php echo $amount; ?></amount>
+		<?php endif; ?>
 		<payment>
 			<creditCard>
+				<?php if (isset($card_number)): ?>
 				<cardNumber><?php echo $card_number; ?></cardNumber>
-				<expirationDate><?php echo $expiration_date; ?></expirationDate>
+				<?php endif; ?>
+				<?php if (isset($card_expiration_date)): ?>
+				<expirationDate><?php echo $card_expiration_date; ?></expirationDate>
+				<?php endif; ?>
+				<?php if (isset($card_code)): ?>
+				<cardCode><?php echo $card_code; ?></cardCode>
+				<?php endif; ?>
 			</creditCard>
 		</payment>
+		<order>
+			<?php if (isset($invoice_number)): ?>
+			<invoiceNumber><?php echo $invoice_number; ?></invoiceNumber>
+			<?php endif; ?>
+			<?php if (isset($invoice_description)): ?>
+			<description><?php echo $invoice_description; ?></description>
+			<?php endif; ?>
+		</order>
+		<customer>
+			<?php if (isset($user_id)): ?>
+			<id><?php echo $user_id; ?></id>
+			<?php endif; ?>
+			<?php if (isset($user_email)): ?>
+			<email><?php echo $user_email; ?></email>
+			<?php endif; ?>
+			<?php if (isset($user_phone)): ?>
+			<phoneNumber><?php echo $user_phone; ?></phoneNumber>
+			<?php endif; ?>
+		</customer>
 		<billTo>
-			<firstName><?php echo $first_name; ?></firstName>
-			<lastName><?php echo $last_name; ?></lastName>
+			<?php if (isset($billing_first_name)): ?>
+			<firstName><?php echo $billing_first_name; ?></firstName>
+			<?php endif; ?>
+			<?php if (isset($billing_last_name)): ?>
+			<lastName><?php echo $billing_last_name; ?></lastName>
+			<?php endif; ?>
 		</billTo>
 	</subscription>
 </ARBUpdateSubscriptionRequest>
