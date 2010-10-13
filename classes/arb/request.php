fcb47a70dd7d641b2a987ffd61e750cd5a229e52
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 04:55:59 -0700

    created the request class that gets extended by all the different request types

---
+++ classes/arb/request.php
@@ -0,0 +1,105 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Arb_Request {
+
+	// URLs to communicate with Auth.net
+	protected $_url = 'https://api.authorize.net/xml/v1/request.api';
+	protected $_dev_url = 'https://apitest.authorize.net/xml/v1/request.api';
+
+	public $test_mode = FALSE;
+
+	protected $_data;
+	protected $_view_path;
+
+	protected $_xml;
+
+	public function __construct()
+	{
+		// Start with the config values
+		$this->_data = Kohana::config('arb')->as_array();
+	}
+
+	public function test_mode($new_mode)
+	{
+		$this->test_mode = $new_mode;
+
+		return $this;
+	}
+
+	public function set($key, $value)
+	{
+		if (is_array($key))
+		{
+			foreach ($key as $k => $v)
+			{
+				$this->__set($k, $v);
+			}
+		}
+		else
+		{
+			$this->__set($key, $value);
+		}
+
+		return $this;
+	}
+
+	public function __set($key, $value)
+	{
+		$this->_data[$key] = $value;
+
+		return $this;
+	}
+
+	public function raw_response()
+	{
+		return $this->_raw_response;
+	}
+
+	public function execute()
+	{
+		// Validating will throw an exception if the object is not ready to execute
+		$this->validate();
+		$this->compile();
+
+		// Decide which API url to use based on whether test_mode was set
+		$url = $this->test_mode ? $this->_dev_url : $this->_url;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, Array('Content-Type: text/xml'));
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $this->_xml);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		$response = curl_exec($ch);
+
+		return new ARB_Response($response);
+	}
+
+	public function compile()
+	{
+		$this->_xml = View::factory($this->_view_path)
+			->set($this->_data)
+			->render();
+
+		return $this;
+	}
+
+	function validation_object()
+	{
+		return Validate::factory($this->_data)
+			->rule('api_login_id', 'not_empty', array())
+			->rule('transaction_key', 'not_empty', array());
+	}
+
+	public function validate()
+	{
+		$object = $this->validation_object();
+
+		if ($object->check())
+			return TRUE;
+
+		throw new ARB_Validation_Exception($object);
+	}
+}
