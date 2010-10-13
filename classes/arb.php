69fc9b3197a1324c9d0b82354267227499f7f7cd
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 05:10:11 -0700

    adding factory methods for the remaining requests

--- classes/arb.php
+++ classes/arb.php
@@ -2,9 +2,23 @@
 
 class Arb {
 
+	public static function create()
+	{
+		return new ARB_Request_Create();
+	}
+
+	public static function update()
+	{
+		return new ARB_Request_Update();
+	}
+
 	public static function status()
 	{
 		return new ARB_Request_Status();
 	}
 
+	public static function cancel()
+	{
+		return new ARB_Request_Cancel();
+	}
 }
