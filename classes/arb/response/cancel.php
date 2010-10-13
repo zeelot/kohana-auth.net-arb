fe08f3d820a730fa71bd8c79a09ba681b55e70c0
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 05:07:35 -0700

    stubbing all the response types

---
+++ classes/arb/response/cancel.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class ARB_Response_Cancel extends ARB_Response {}
---
+++ classes/arb/response/create.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class ARB_Response_Create extends ARB_Response {}
