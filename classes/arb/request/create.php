94a57303849767bfbb7ffb503704922514f8529a
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 05:07:23 -0700

    stubbing the create and update requests

---
+++ classes/arb/request/create.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class ARB_Request_Create extends ARB_Request {
+
+	protected $_view_path = 'auth-arb/create_subscription.xml';
+}
---
+++ classes/arb/request/update.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class ARB_Request_Update extends ARB_Request {
+
+	protected $_view_path = 'auth-arb/update_subscription.xml';
+}
