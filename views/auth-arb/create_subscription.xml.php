f659c7cf02f67a94be4d5b3d0f3fcaebcf02de15
Lorenzo Pisani <zeelot3k@gmail.com>
2010-10-13 06:20:33 -0700

    fixing the xml for creating a subscription

--- views/auth-arb/create_subscription.xml.php
+++ views/auth-arb/create_subscription.xml.php
@@ -22,13 +22,17 @@ echo "<?xml version='1.0' encoding='utf-8'?>"; ?>
 		<name><?php echo $api_login_id; ?></name>
 		<transactionKey><?php echo $transaction_key; ?></transactionKey>
 	</merchantAuthentication>
+	<?php if (isset($ref_id)): ?>
 	<refId><?php echo $ref_id; ?></refId>
+	<?php endif; ?>
 	<subscription>
-		<name><?php echo $name; ?></name>
+		<?php if (isset($subscription_name)): ?>
+		<name><?php echo $subscription_name; ?></name>
+		<?php endif; ?>
 		<paymentSchedule>
 			<interval>
-				<length><?php echo $length; ?></length>
-				<unit><?php echo $unit; ?></unit>
+				<length><?php echo $interval_length; ?></length>
+				<unit><?php echo $interval_unit; ?></unit>
 			</interval>
 			<startDate><?php echo $start_date; ?></startDate>
 			<totalOccurrences><?php echo $total_occurrences; ?></totalOccurrences>
@@ -37,12 +41,34 @@ echo "<?xml version='1.0' encoding='utf-8'?>"; ?>
 		<payment>
 			<creditCard>
 				<cardNumber><?php echo $card_number; ?></cardNumber>
-				<expirationDate><?php echo $expiration_date; ?></expirationDate>
+				<expirationDate><?php echo $card_expiration_date; ?></expirationDate>
+				<?php if (isset($card_code)): ?>
+				<cardCode><?php echo $card_code; ?></cardCode>
+				<?php endif; ?>
 			</creditCard>
 		</payment>
+		<order>
+			<?php if (isset($invoice_number)): ?>
+			<invoiceNumber><?php echo $invoice_number; ?></invoiceNumber>
+			<?php endif; ?>
+			<?php if (isset($invoice_description)): ?>
+			<description><?php echo $invoice_description; ?></description>
+			<?php endif; ?>
+		</order>
+		<customer>
+			<?php if (isset($user_id)): ?>
+			<id><?php echo $user_id; ?></id>
+			<?php endif; ?>
+			<?php if (isset($user_email)): ?>
+			<email><?php echo $user_email; ?></email>
+			<?php endif; ?>
+			<?php if (isset($user_phone)): ?>
+			<phoneNumber><?php echo $user_phone; ?></phoneNumber>
+			<?php endif; ?>
+		</customer>
 		<billTo>
-			<firstName><?php echo $first_name; ?></firstName>
-			<lastName><?php echo $last_name; ?></lastName>
+			<firstName><?php echo $billing_first_name; ?></firstName>
+			<lastName><?php echo $billing_last_name; ?></lastName>
 		</billTo>
 	</subscription>
 </ARBCreateSubscriptionRequest>
